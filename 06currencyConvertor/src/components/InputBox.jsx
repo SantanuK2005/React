4b2cb6177cb7adda280da7ab42c925e3be46436f
Santanu Khorat <santanukhorat@gmail.com>
2025-08-24 11:34:26 +0530

    Currency Converter App

--- 06currencyConvertor/src/components/InputBox.jsx
+++ 06currencyConvertor/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React,{useId} from "react";
+import React, {useId} from 'react'
 
 function InputBox({
     label,
@@ -9,19 +9,18 @@ function InputBox({
     selectCurrency = "usd",
     amountDisable = false,
     currencyDisable = false,
-
     className = "",
 }) {
-   const amuountInputId = useId()
+   const amountInputId = useId()
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label htmlFor={amuountInputId} className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    id={amuountInputId}
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
@@ -38,10 +37,10 @@ function InputBox({
                     onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                     disabled={currencyDisable}
                 >
-                    {currencyOptions.map((currency) => (
-                        <option key={currency} 
-                                value={currency}>
-                            
+                    
+                        {currencyOptions.map((currency) => (
+                            <option key={currency} value={currency}>
+                            {currency}
                             </option>
                         ))}
                 
