3e90507ba2ae63506e6ad1f745c8619b4b06774a
Santanu Khorat <santanukhorat@gmail.com>
2025-08-28 12:46:41 +0530

    Theme Swicher

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,40 +1,43 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeButton'
+import Card from './components/Card'
 
 function App() {
-  const [themeMode, setThemeMode] = useState("light");
-
-  const darkTheme = () => {
-    setThemeMode("dark");
-  }
+  const [themeMode, setThemeMode] = useState("light")
 
   const lightTheme = () => {
-    setThemeMode("light");
+    setThemeMode("light")
   }
 
-  //Actual implementation of the theme switcher
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // actual change in theme
 
   useEffect(() => {
-    document.querySelector("html").classList.remove("light", "dark");
-    document.querySelector("html").classList.add(themeMode);
-  }, [themeMode]);
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
 
   return (
-    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                <ThemeButton />
-            </div>
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
 
-            <div className="w-full max-w-sm mx-auto">
-                <Card />
-            </div>
-        </div>
-    </div>
-  </ThemeProvider>
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
