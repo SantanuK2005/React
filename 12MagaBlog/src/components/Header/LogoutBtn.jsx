584a63a6788b2c6f4fa58cba99d1fc757db7aa6c
Santanu Khorat <santanukhorat@gmail.com>
2025-09-04 02:49:58 +0530

    Maga Blog Project

--- 12MagaBlog/src/components/Header/LogoutBtn.jsx
+++ 12MagaBlog/src/components/Header/LogoutBtn.jsx
@@ -14,7 +14,8 @@ function LogoutBtn() {
     };
 
     return (
-        <div>LogoutBtn</div>
+        <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+            Logout</button>
     )
  }
 
--- 12MagaBlog/src/components/index.js
+++ 12MagaBlog/src/components/index.js
@@ -1,4 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import container from "./Container/Container";
+import Logo from "./logo.jsx";
 
-export { Header, Footer };
+export { Header, Footer, container, Logo };
