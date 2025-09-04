57f439c3e529a94c4d23b8c4290cad9020597de3
Santanu Khorat <santanukhorat@gmail.com>
2025-09-05 00:41:24 +0530

    Maga Blog Project

--- 12MagaBlog/src/components/Select.jsx
+++ 12MagaBlog/src/components/Select.jsx
@@ -11,7 +11,7 @@ function Select({
             {...props}
             id={id}
             ref={ref}
-            className={``}
+            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
             >
                 {options?.map((options) => (
                     <option key={options} value={options}>
