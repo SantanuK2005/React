f6559f235e84992dcebdd351dbb2a43ba46b3ecb
Santanu Khorat <santanukhorat@gmail.com>
2025-09-08 23:54:58 +0530

    Maga Blog Project

--- 12MagaBlog/src/appwrite/config.js
+++ 12MagaBlog/src/appwrite/config.js
@@ -1,20 +1,22 @@
-import conf from '../conf.js';
-import { Client, Account, ID, Databases, Storage, Query } from 'appwrite';
+
+import conf from '../conf/conf.js';
+import { Client, ID, Databases, Storage, Query } from "appwrite";
 
 export class Service{
     client = new Client();
     databases;
     bucket;
-
+    
     constructor(){
         this.client
         .setEndpoint(conf.appwriteUrl)
-        .setProject(conf.appwriteProjectId); 
+        .setProject(conf.appwriteProjectId);
         this.databases = new Databases(this.client);
         this.bucket = new Storage(this.client);
     }
-    async createPost(title, slug, content, featuredImage, status, userId){
-        try{
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
             return await this.databases.createDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
@@ -24,15 +26,16 @@ export class Service{
                     content,
                     featuredImage,
                     status,
+                    userId,
                 }
-            );
-        }catch(error){
-            console.error("Appwrite service :: createPost :: error", error);
-            throw error;
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: createPost :: error", error);
         }
     }
-    async UpdatePost(slug, title, content, featuredImage, status, ){
-        try{
+
+    async updatePost(slug, {title, content, featuredImage, status}){
+        try {
             return await this.databases.updateDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
@@ -41,50 +44,56 @@ export class Service{
                     title,
                     content,
                     featuredImage,
-                    status
+                    status,
+
                 }
-            );
-        } catch(error){
-            console.error("Appwrite service :: updatePost :: error", error);
-            throw error;
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: updatePost :: error", error);
         }
     }
+
     async deletePost(slug){
-        try{
+        try {
             await this.databases.deleteDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug
-            );
-            return true;
-        } catch(error){
-            console.error("Appwrite service :: deletePost :: error", error);
-            return false;
+            
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite serive :: deletePost :: error", error);
+            return false
         }
     }
+
     async getPost(slug){
-        try{
+        try {
             return await this.databases.getDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug
-            );
-        } catch(error){
-            console.error("Appwrite service :: getPost :: error", error);
-            return false;
+            
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: getPost :: error", error);
+            return false
         }
     }
+
     async getPosts(queries = [Query.equal("status", "active")]){
         try {
             return await this.databases.listDocuments(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 queries,
+                
 
-            );
+            )
         } catch (error) {
-            console.log("Appwrite service :: getPosts :: error", error);
-            return false;
+            console.log("Appwrite serive :: getPosts :: error", error);
+            return false
         }
     }
 
@@ -96,11 +105,10 @@ export class Service{
                 conf.appwriteBucketId,
                 ID.unique(),
                 file
-            );
-            return response;
+            )
         } catch (error) {
-            console.error("Appwrite service :: uploadFile :: error", error);
-            return false;
+            console.log("Appwrite serive :: uploadFile :: error", error);
+            return false
         }
     }
 
@@ -109,20 +117,22 @@ export class Service{
             await this.bucket.deleteFile(
                 conf.appwriteBucketId,
                 fileId
-            );
-            return true;
+            )
+            return true
         } catch (error) {
-            console.error("Appwrite service :: deleteFile :: error", error);
-            return false;
+            console.log("Appwrite serive :: deleteFile :: error", error);
+            return false
         }
     }
 
-    async getFilePreview(fileId){
+    getFilePreview(fileId){
         return this.bucket.getFilePreview(
             conf.appwriteBucketId,
             fileId
-        );
+        )
     }
 }
-const service = new Service();
-export default service;
+
+
+const service = new Service()
+export default service
