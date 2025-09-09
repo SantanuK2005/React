75beb5ad5038fbe98f81e35a31625ede85de6058
Santanu Khorat <santanukhorat@gmail.com>
2025-09-09 11:48:19 +0530

    Maga Blog Project

---
+++ 12MagaBlog/src/pages/AddPost.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container as PostForm} from "../components";
+
+function AddPost() {
+    return (
+        <div>
+            <Container >
+                <PostForm />
+            </Container >
+        </div>
+    );
+}
+
+export default AddPost;
---
+++ 12MagaBlog/src/pages/Signuo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Signup as SignuoComponents } from "../components";
+
+function Signup() {
+    return (
+        <div>
+            <SignuoComponents />
+        </div>
+    );
+}
+
+export default Signup;  
